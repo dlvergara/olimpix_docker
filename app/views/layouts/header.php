459172dd539849f776c0444d52b4ca2b268185ab
David Vergara <david.l.vergara@gmail.com>
2019-04-19 00:11:31 -0500

    site url and event url

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -1,7 +1,7 @@
 <?php
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
-use yii\helpers\Html;
+use yii\helpers\Html;use yii\helpers\Url;
 
 /*
 NavBar::begin([
@@ -39,12 +39,14 @@ NavBar::begin([
     <div class="container">
         <div class="row header-top align-items-center">
             <div class="col-lg-4 col-sm-4 menu-top-left">
-                <a href="mailto:info@horseclub.com"><span class="lnr lnr-location"></span></a>
+                <a href="mailto:info@horseclub.com">
+                    <span class="lnr lnr-location"></span>
+                </a>
                 <a class="tel" href="mailto:info@horseclub.com">info@horseclub.com</a>
             </div>
             <div class="col-lg-4 menu-top-middle justify-content-center d-flex">
-                <a href="index.html">
-                    <img class="img-fluid" src="img/logo.png" alt="">
+                <a href="<?= Yii::$app->getHomeUrl() ?>/">
+                    <img class="img-fluid" src="<?= Url::base(true) ?>/img/logo.png" alt="">
                 </a>
             </div>
             <div class="col-lg-4 col-sm-4 menu-top-right">
@@ -58,11 +60,11 @@ NavBar::begin([
         <div class="row align-items-center justify-content-center d-flex">
           <nav id="nav-menu-container">
             <ul class="nav-menu">
-              <li class="menu-active"><a href="index.html">Home</a></li>
+              <li class="menu-active"><a href="<?= Yii::$app->getHomeUrl() ?>">Home</a></li>
               <li><a href="about.html">About</a></li>
               <li><a href="service.html">Service</a></li>
               <li><a href="training.html">training</a></li>
-              <li><a href="events.html">events</a></li>
+              <li><a href="<?= Yii::$app->getUrlManager()->createUrl(["site/events"]) ?>">events</a></li>
               <li><a href="pricing.html">Pricing</a></li>
               <li class="menu-has-children"><a href="">Blog</a>
                 <ul>
