04caba05d4687e01a2ee66433189d3bcfa9529a3
David Vergara <david.l.vergara@gmail.com>
2019-04-18 23:12:23 -0500

    add js includes

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -70,6 +70,24 @@ AppAsset::register($this);
     $this->render("footer");
     ?>
 
+    <script src="<?= Url::base(true) ?>/js/vendor/jquery-2.2.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="<?= Url::base(true) ?>/js/vendor/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+    <script src="<?= Url::base(true) ?>/js/easing.min.js"></script>
+    <script src="<?= Url::base(true) ?>/js/hoverIntent.js"></script>
+    <script src="<?= Url::base(true) ?>/js/superfish.min.js"></script>
+    <script src="<?= Url::base(true) ?>/js/jquery.ajaxchimp.min.js"></script>
+    <script src="<?= Url::base(true) ?>/js/jquery.magnific-popup.min.js"></script>
+    <script src="<?= Url::base(true) ?>/js/owl.carousel.min.js"></script>
+    <script src="<?= Url::base(true) ?>/js/jquery.sticky.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="<?= Url::base(true) ?>/js/jquery.nice-select.min.js"></script>
+    <script src="<?= Url::base(true) ?>/js/parallax.min.js"></script>
+    <script src="<?= Url::base(true) ?>/js/waypoints.min.js"></script>
+    <script src="<?= Url::base(true) ?>/js/jquery.counterup.min.js"></script>
+    <script src="<?= Url::base(true) ?>/js/mail-script.js"></script>
+    <script src="<?= Url::base(true) ?>/js/main.js"></script>
 
 <?php $this->endBody() ?>
 </body>
