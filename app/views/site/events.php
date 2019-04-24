29b3d15867a32781d4944dc491863ec9ea68caa4
David Vergara <david.l.vergara@gmail.com>
2019-04-23 19:46:03 -0500

    ejemplo

--- app/views/site/events.php
+++ app/views/site/events.php
@@ -32,7 +32,6 @@ $this->params['breadcrumbs'][] = $this->title;
         setlocale(LC_ALL,"es_ES");
         foreach ($dataProvider->getModels() as $index => $model) {
             $date = DateTime::createFromFormat("Y-m-d H:i:s", $model->fecha_inicio);
-
             $eventoNombre = $model->nombre;
             $eventoFecha = strftime("%A %d de %B", $date->getTimestamp());
             $eventoLugar = $model->ciudadIdCiudad->nombre;
