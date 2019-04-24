5ff5f7d29ef610bb394d30f050abfe0db12c47d7
Edwin Jair Bernal Roldan <ing.edwin.bernal@gmail.com>
2019-04-23 19:25:35 -0500

    CCoammit inicial

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -6,34 +6,37 @@
 					<div class="row">
 						<div class="col-lg-3  col-md-6 col-sm-6">
 							<div class="single-footer-widget">
-								<h4>About Us</h4>
+								<h4>Sobre Nosotros</h4>
 								<p>
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
+									Empresa lider en el mercado que brinda soluciones Software as a Service (SaaS) o Software como un Servicio, abreviadamente (ScuS) para eventos ecuestres.
 								</p>
 							</div>
 						</div>
 						<div class="col-lg-4  col-md-6 col-sm-6">
 							<div class="single-footer-widget">
-								<h4>Contact Us</h4>
+								<h4>Contáctenos</h4>
 								<p>
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.
+									Te invitamos a contactarnos si tienes inquietudes sobre Olimpix.
 								</p>
 								<p class="number">
-									012-6532-568-9746 <br>
-									012-6532-569-9748
+									031-8796572 <br>
+									
 								</p>
 							</div>
 						</div>
 						<div class="col-lg-5  col-md-6 col-sm-6">
 							<div class="single-footer-widget">
-								<h4>Newsletter</h4>
-								<p>You can trust us. we only send  offers, not a single spam.</p>
+								<h4>News</h4>
+								<p>Tenemos una sección de Preguntas Frecuentes a la que puedes dirigirte en primera instancia y donde encontrarás las respuestas a las principales inquietudes.</p>
+                                <p>
+                                Si persisten tus dudas, déjanos tus datos y la respectiva inquietud para darte una respuesta personalizada.
+                                </p>
 								<div class="d-flex flex-row" id="mc_embed_signup">
 
 
 									  <form class="navbar-form" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get">
 									    <div class="input-group add-on align-items-center d-flex">
-									      	<input class="form-control" name="EMAIL" placeholder="Email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email address'" required="" type="email">
+									      	<input class="form-control" name="EMAIL" placeholder="Email address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" required="" type="email">
 											<div style="position: absolute; left: -5000px;">
 												<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
 											</div>
@@ -50,14 +53,14 @@
 					<div class="footer-bottom row">
 						<p class="footer-text m-0 col-lg-6 col-md-12">
 							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Ese Sitio esta hecho con Mucho "<i class="fa fa-heart-o" aria-hidden="true"></i> " <a href="https://olimpix.com.co" target="_blank">Olimpix</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 						</p>
 						<div class="footer-social col-lg-6 col-md-12">
 							<a href="#"><i class="fa fa-facebook"></i></a>
 							<a href="#"><i class="fa fa-twitter"></i></a>
 							<a href="#"><i class="fa fa-dribbble"></i></a>
-							<a href="#"><i class="fa fa-behance"></i></a>
+						<!--	<a href="#"><i class="fa fa-behance"></i></a> -->
 						</div>
 					</div>
 				</div>
--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -34,24 +34,33 @@ NavBar::begin([
     NavBar::end();
     */
 ?>
+
 <!-- HEADER -->
 <header id="header" id="home">
+    
+    
     <div class="container">
         <div class="row header-top align-items-center">
             <div class="col-lg-4 col-sm-4 menu-top-left">
                 <a href="mailto:info@horseclub.com">
                     <span class="lnr lnr-location"></span>
                 </a>
-                <a class="tel" href="mailto:info@horseclub.com">info@horseclub.com</a>
+                <a class="tel" href="mailto:info@horseclub.com">info@olimpix.com.co</a>
             </div>
             <div class="col-lg-4 menu-top-middle justify-content-center d-flex">
                 <a href="<?= Yii::$app->getHomeUrl() ?>/">
-                    <img class="img-fluid" src="<?= Url::base(true) ?>/img/logo.png" alt="">
+                    <img class="img-fluid" src="<?= Url::base(true) ?>/img/Logo-2.png" alt="">
                 </a>
             </div>
             <div class="col-lg-4 col-sm-4 menu-top-right">
-                <a class="tel" href="tel:+880 123 12 658 439">+880 123 12 658 439</a>
+                <a class="tel" href="tel:+880 123 12 658 439">+031 87966572</a>
                 <a href="tel:+880 123 12 658 439"><span class="lnr lnr-phone-handset"></span></a>
+            </div>
+            <div class="btn-group">
+            <button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>
+               <div id="mobile-nav-toggle" style="display: block;"></div>
+                
+               
             </div>
         </div>
     </div>
@@ -60,23 +69,25 @@ NavBar::begin([
         <div class="row align-items-center justify-content-center d-flex">
           <nav id="nav-menu-container">
             <ul class="nav-menu">
-              <li class="menu-active"><a href="<?= Yii::$app->getHomeUrl() ?>">Home</a></li>
-              <li><a href="about.html">About</a></li>
-              <li><a href="service.html">Service</a></li>
-              <li><a href="training.html">training</a></li>
-              <li><a href="<?= Yii::$app->getUrlManager()->createUrl(["site/events"]) ?>">events</a></li>
-              <li><a href="pricing.html">Pricing</a></li>
+              <li class="menu-active"><a href="<?= Yii::$app->getHomeUrl() ?>">Inicio</a></li>
+              <li><a href="about.html">Nosotros</a></li>
+              <li><a href="service.html">Servicios</a></li>
+              <li><a href="training.html">formación</a></li>
+              <li><a href="<?= Yii::$app->getUrlManager()->createUrl(["site/events"]) ?>">eventos</a></li>
+              <li><a href="pricing.html">Precios</a></li>
               <li class="menu-has-children"><a href="">Blog</a>
                 <ul>
                   <li><a href="blog-home.html">Blog Home</a></li>
                   <li><a href="blog-single.html">Blog Single</a></li>
                 </ul>
               </li>
-              <li><a href="contact.html">Contact</a></li>
-              <li><a href="<?= Yii::$app->getUrlManager()->createUrl(["site/elements"]) ?>">Elements</a></li>
+              <li><a href="contact.html">Contacto</a></li>
+              <li><a href="<?= Yii::$app->getUrlManager()->createUrl(["site/elements"]) ?>">Elementos</a></li>
             </ul>
           </nav><!-- #nav-menu-container -->
         </div>
     </div>
+    
+    
 </header>
 <!-- #header -->
