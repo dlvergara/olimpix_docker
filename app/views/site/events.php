de3a5466301eafbec6eb05d573cccf525de2c44b
David Vergara <david.l.vergara@gmail.com>
2019-04-25 22:04:23 -0500

    show more events

--- app/views/site/events.php
+++ app/views/site/events.php
@@ -24,14 +24,15 @@ $this->params['breadcrumbs'][] = $this->title;
                 <p>
                     Revisa el calendario deportivo
                 </p>
-            
+
             </div>
         </div>
         <?php
         /* @var $model app\models\Evento */
-        setlocale(LC_ALL,"es_ES");
+        setlocale(LC_ALL, "es_ES");
         foreach ($dataProvider->getModels() as $index => $model)
         {
+            $par = ($index % 2 == 0) ? 1 : 2;
             $date = DateTime::createFromFormat("Y-m-d H:i:s", $model->fecha_inicio);
             $eventoNombre = $model->nombre;
             $eventoFecha = strftime("%A %d de %B", $date->getTimestamp());
@@ -41,7 +42,7 @@ $this->params['breadcrumbs'][] = $this->title;
             ?>
             <div class="row align-items-center upcoming-wrap">
                 <div class="col-lg-6 upcoming-left">
-                    <img class="img-fluid" src="img/pages/ev1.jpg" alt="">
+                    <img class="img-fluid" src="img/pages/ev<?= $par ?>.jpg" alt="">
                 </div>
                 <div class="col-lg-6 upcoming-right">
                     <a href="#"><h4><?= $eventoNombre ?></h4></a>
@@ -54,95 +55,26 @@ $this->params['breadcrumbs'][] = $this->title;
                     <a class="primary-btn text-uppercase" href="<?= $eventoUrl ?>">ver detalles</a>
                 </div>
             </div>
-        
-     <!--  <section class="upcoming-event-area section-gap">-->
-        <div class="container">
-            <div class="row d-flex justify-content-center">
-             <div class="col-md-9 pb-40 header-text text-center">
-        
-                <h2 class="pb-10">Revisa el calendario Ecuestre 2019</h2>
-        
-             <div class="embed-responsive embed-responsive-16by9">
-                 <iframe class="embed-responsive-item" src="http://www.fedecuestre.com/images/noticias/2019/CALENDARIO_NACIONAL_2019.pdf" allowfullscreen></iframe>
-            </div>
-         </div>
-        </div>
-  <!--   </section>
-            
             <?php
         }
         ?>
-        <!--
-                        <div class="row align-items-center upcoming-wrap">
-                            <div class="col-lg-6 upcoming-left">
-                                <img class="img-fluid" src="img/pages/ev1.jpg" alt="">
-                            </div>
-                            <div class="col-lg-6 upcoming-right">
-                                <a href="#"><h4>Event on the rock solid carbon</h4></a>
-                                <p class="meta">
-                                    <span>21st February</span>
-                                    at Central government museum
-                                </p>
-                                <p>
-                                    inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially.
-                                </p>
-                                <a class="primary-btn text-uppercase" href="#">view details</a>
-                            </div>
-                        </div>
 
-                        <div class="row align-items-center upcoming-wrap">
-                            <div class="col-lg-6 upcoming-left">
-                                <a href="#"><h4>Event on the rock solid carbon</h4></a>
-                                <p class="meta">
-                                    <span>21st February</span>
-                                    at Central government museum
-                                </p>
-                                <p>
-                                    inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially.
-                                </p>
-                                <a class="primary-btn text-uppercase" href="#">view details</a>
-                            </div>
-                            <div class="col-lg-6 upcoming-right">
-                                <img class="img-fluid" src="img/pages/ev2.jpg" alt="">
-                            </div>
-                        </div>
+        <!--  <section class="upcoming-event-area section-gap">-->
+        <div class="container">
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-9 pb-40 header-text text-center">
 
-                        <div class="row align-items-center upcoming-wrap">
-                            <div class="col-lg-6 upcoming-left">
-                                <img class="img-fluid" src="img/pages/ev3.jpg" alt="">
-                            </div>
-                            <div class="col-lg-6 upcoming-right">
-                                <a href="#"><h4>Event on the rock solid carbon</h4></a>
-                                <p class="meta">
-                                    <span>21st February</span>
-                                    at Central government museum
-                                </p>
-                                <p>
-                                    inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially.
-                                </p>
-                                <a class="primary-btn text-uppercase" href="#">view details</a>
-                            </div>
-                        </div>
-                        <div class="row align-items-center upcoming-wrap">
-                            <div class="col-lg-6 order-1 upcoming-left">
-                                <a href="#"><h4>Event on the rock solid carbon</h4></a>
-                                <p class="meta">
-                                    <span>21st February</span>
-                                    at Central government museum
-                                </p>
-                                <p>
-                                    inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially.
-                                </p>
-                                <a class="primary-btn text-uppercase" href="#">view details</a>
-                            </div>
-                            <div class="col-lg-6 order-2 upcoming-right">
-                                <img class="img-fluid" src="img/pages/ev4.jpg" alt="">
-                            </div>
-                        </div>
+                    <h2 class="pb-10">Revisa el calendario Ecuestre 2019</h2>
+
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item"
+                                src="http://www.fedecuestre.com/images/noticias/2019/CALENDARIO_NACIONAL_2019.pdf"
+                                allowfullscreen></iframe>
                     </div>
-        -->
-         </div>
-        
+                </div>
+            </div>
+        </div>
     </div>
+
 </section>
 <!-- End upcoming-event Area -->
