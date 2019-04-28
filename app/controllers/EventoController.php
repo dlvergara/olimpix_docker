2eff49dcad4a87815997e37ff95dff7475662b18
David Vergara <david.l.vergara@gmail.com>
2019-04-28 00:54:18 -0500

    mostrar total

--- app/controllers/EventoController.php
+++ app/controllers/EventoController.php
@@ -3,17 +3,62 @@
 namespace app\controllers;
 
 use app\models\Evento;
+use app\models\LoginForm;
 use app\models\ReservaForm;
+use Yii;
 use yii\web\NotFoundHttpException;
+use yii\widgets\ActiveForm;
 
 class EventoController extends \yii\web\Controller
 {
+    protected $models;
+    protected $validModels;
+
     public function actionIndex($evento)
     {
-        $formModel = new ReservaForm();
-
         $model = $this->findModel($evento);
-        return $this->render('index', ['model' => $model, 'formModel' => $formModel]);
+        return $this->render('index', ['model' => $model,]);
+    }
+
+    /*
+     * Action reservar
+     */
+    public function actionReservar($evento)
+    {
+        $eventoModel = $this->findModel($evento);
+        if (Yii::$app->request->post() && $this->validarReserva()) {
+            $sumaMonto = 0;
+            /* @var $validModel ReservaForm */
+            foreach ($this->validModels as $index => $validModel) {
+                $sumaMonto += $validModel->getServicioDisponible()->monto * $validModel->cantidad;
+            }
+        }
+        return $this->render('reservar-servicios', ['model' => $eventoModel, 'formModels' => $this->models, 'subtotal' => $sumaMonto]);
+    }
+
+    /**
+     *
+     */
+    private function validarReserva()
+    {
+        $this->models = [];
+        $postData = Yii::$app->request->post();
+        foreach ($postData['ReservaForm'] as $index => $postDatum) {
+            $formModel = new ReservaForm();
+            $formModel->cantidad = $postDatum['cantidad'];
+            $formModel->servicio = isset($postDatum['servicio']) ? $postDatum['servicio'] : null;
+            $this->models[$formModel->servicio] = $formModel;
+        }
+
+        /* @var $model ReservaForm */
+        foreach ($this->models as $index => $model) {
+            if (!empty($index)) {
+                if ($model->validate() && $model->getServicioModel()) {
+                    $this->validModels[] = $model;
+                }
+            }
+        }
+        return !empty($this->validModels);
     }
 
     /**
