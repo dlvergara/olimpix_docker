05c80ee119d895bcee42a0997eb041e370e72b43
Edwin Jair Bernal Roldan <ing.edwin.bernal@gmail.com>
2019-05-05 22:17:53 -0500

    Se suben fotos Coprogreso

--- app/views/site/galeria.php
+++ app/views/site/galeria.php
@@ -17,6 +17,79 @@ $this->title = 'Galeria';
             <h2>Eventos recientes </h2>
         </div>
         <div class="row no-gutters">
+            
+            
+            <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h1.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h1.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Club la Hacienda</span>
+                            <span class="description-body">Febrero 3 y 4 - Concurso de salto en la Hacienda Club, Cajicá</span>
+                        </span>
+                </a>
+            </div>
+            
+                <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h5.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h5.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Club la Hacienda</span>
+                            <span class="description-body">Febrero 3 y 4 - Concurso de salto en la Hacienda Club, Cajicá</span>
+                        </span>
+                </a>
+            </div>
+            
+            
+               <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h3.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h3.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Club la Hacienda</span>
+                            <span class="description-body">Febrero 3 y 4 - Concurso de salto en la Hacienda Club, Cajicá</span>
+                        </span>
+                </a>
+            </div>
+
+
+            <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h4.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h4.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Club la Hacienda</span>
+                            <span class="description-body">Febrero 3 y 4 - Concurso de salto en la Hacienda Club, Cajicá</span>
+                        </span>
+                </a>
+            </div>
+
+              <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h2.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h2.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Club la Hacienda</span>
+                            <span class="description-body">Febrero 3 y 4 - Concurso de salto en la Hacienda Club, Cajicá</span>
+                        </span>
+                </a>
+            </div>
+        
+            <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h6.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h6.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Club la Hacienda</span>
+                            <span class="description-body">Febrero 3 y 4 - Concurso de salto en la Hacienda Club, Cajicá</span>
+                        </span>
+                </a>
+            </div>
+            
+            <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p1.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p1.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Polo Club</span>
+                            <span class="description-body">Primer encuentro Ecuestre Copa Coprogreso 2019</span>
+                        </span>
+                </a>
+            </div>
 
             <div class="col-md-6 col-lg-4 item zoom-on-hover">
                 <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p2.jpg' ?>">
@@ -37,16 +110,7 @@ $this->title = 'Galeria';
                 </a>
             </div>
 
-            <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p5.jpg' ?>">
-                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p5.jpg' ?>">
-                    <span class="description">
-                            <span class="description-heading">Polo Club</span>
-                            <span class="description-body">Primer encuentro Ecuestre Copa Coprogreso 2019</span>
-                        </span>
-                </a>
-            </div>
-
+            
 
             <div class="col-md-6 col-lg-4 item zoom-on-hover">
                 <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p4.jpg' ?>">
@@ -57,50 +121,10 @@ $this->title = 'Galeria';
                         </span>
                 </a>
             </div>
-
-            <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h3.jpg' ?>">
-                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h3.jpg' ?>">
-                    <span class="description">
-                            <span class="description-heading">Club la Hacienda</span>
-                            <span class="description-body">Febrero 3 y 4 - Concurso de salto en la Hacienda Club, Cajicá</span>
-                        </span>
-                </a>
-            </div>
-
-
-            <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h1.jpg' ?>">
-                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h1.jpg' ?>">
-                    <span class="description">
-                            <span class="description-heading">Club la Hacienda</span>
-                            <span class="description-body">Febrero 3 y 4 - Concurso de salto en la Hacienda Club, Cajicá</span>
-                        </span>
-                </a>
-            </div>
-            <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h2.jpg' ?>">
-                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h2.jpg' ?>">
-                    <span class="description">
-                            <span class="description-heading">Club la Hacienda</span>
-                            <span class="description-body">Febrero 3 y 4 - Concurso de salto en la Hacienda Club, Cajicá</span>
-                        </span>
-                </a>
-            </div>
-
-            <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h4.jpg' ?>">
-                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/h4.jpg' ?>">
-                    <span class="description">
-                            <span class="description-heading">Club la Hacienda</span>
-                            <span class="description-body">Febrero 3 y 4 - Concurso de salto en la Hacienda Club, Cajicá</span>
-                        </span>
-                </a>
-            </div>
-
-            <div class="col-md-6 col-lg-4 item zoom-on-hover">
-                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p1.jpg' ?>">
-                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p1.jpg' ?>">
+            
+                <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p6.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p6.jpg' ?>">
                     <span class="description">
                             <span class="description-heading">Polo Club</span>
                             <span class="description-body">Primer encuentro Ecuestre Copa Coprogreso 2019</span>
@@ -108,6 +132,80 @@ $this->title = 'Galeria';
                 </a>
             </div>
 
+        
+
+            
+                <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p7.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p7.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Polo Club</span>
+                            <span class="description-body">Primer encuentro Ecuestre Copa Coprogreso 2019</span>
+                        </span>
+                </a>
+            </div>
+            
+                <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p8.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p8.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Polo Club</span>
+                            <span class="description-body">Primer encuentro Ecuestre Copa Coprogreso 2019</span>
+                        </span>
+                </a>
+            </div>
+            
+                <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p9.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p9.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Polo Club</span>
+                            <span class="description-body">Primer encuentro Ecuestre Copa Coprogreso 2019</span>
+                        </span>
+                </a>
+            </div>
+            
+                <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p12.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p12.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Polo Club</span>
+                            <span class="description-body">Primer encuentro Ecuestre Copa Coprogreso 2019</span>
+                        </span>
+                </a>
+            </div>
+            
+                <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p14.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p14.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Polo Club</span>
+                            <span class="description-body">Primer encuentro Ecuestre Copa Coprogreso 2019</span>
+                        </span>
+                </a>
+            </div>
+            
+                <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p15.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p15.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Polo Club</span>
+                            <span class="description-body">Primer encuentro Ecuestre Copa Coprogreso 2019</span>
+                        </span>
+                </a>
+            </div>
+                <div class="col-md-6 col-lg-4 item zoom-on-hover">
+                <a class="lightbox" href="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p16.jpg' ?>">
+                    <img class="img-fluid image" src="<?= Yii::$app->getUrlManager()->baseUrl.'/img/p16.jpg' ?>">
+                    <span class="description">
+                            <span class="description-heading">Polo Club</span>
+                            <span class="description-body">Primer encuentro Ecuestre Copa Coprogreso 2019</span>
+                        </span>
+                </a>
+            </div>
+            
+          
+             
 
         </div>
     </div>
