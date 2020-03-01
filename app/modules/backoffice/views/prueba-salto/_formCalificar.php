1f0df8b60730635bf1eba13a32d1020c4fc37937
David Vergara <david.l.vergara@gmail.com>
2020-02-29 20:11:42 -0500

    tabla para la calificacion

--- app/modules/backoffice/views/prueba-salto/_formCalificar.php
+++ app/modules/backoffice/views/prueba-salto/_formCalificar.php
@@ -26,8 +26,49 @@ $form = ActiveForm::begin([
     'options' => ['data-pjax' => '1', 'class' => 'row'],
 ]);
 ?>
-    <div class="col-md-2"><?= $row->orden_participacion . '. ' . $row->caballoHasJinete->jinete->nombre_completo ?></div>
-<div class="col-md-1"><?= $row->caballoHasJinete->caballo->nombre ?></div>
+    <table class="table-responsive" border="1px solid black">
+        <tbody>
+        <tr>
+            <td class="col-md-1"><?= $row->orden_participacion ?></td>
+            <td class="col-md-2"
+                style="width 300px"><?= ucwords(strtolower($row->caballoHasJinete->jinete->nombre_completo)) ?></td>
+            <td class="col-md-1"><?= ucwords(strtolower($row->caballoHasJinete->caballo->nombre)) ?></td>
+            <td class="col-md-1"><?= $form->field($row, 'falta_obst')->textInput(['type' => 'number'])->label(false) ?></td>
+            <td class="col-md-1"><?= $form->field($row, 'tiempo')->textInput(['type' => 'number', 'step' => '0.01'])->label(false) ?></td>
+            <td class="col-md-1"><?= $form->field($row, 'faltas_tiempo')->textInput(['type' => 'number'])->label(false) ?></td>
+            <td class="col-md-1"><?= $form->field($row, 'faltas_totales')->textInput(['type' => 'number'])->label(false) ?></td>
+            <td class="col-md-1"><?= $form->field($row, 'clasificacion')->textInput(['type' => 'number'])->label(false) ?></td>
+            <td class="col-md-1"><?= $form->field($row, 'observaciones')->textInput(['rows' => 6])->label(false) ?></td>
+            <td class="col-md-1"><?= $form->field($row, 'cantidad_rehuso')->textInput(['type' => 'number'])->label(false) ?></td>
+            <td class="col-md-1"><?= $form->field($row, 'eliminado')->checkbox()->label(false) ?></td>
+            <td class="col-md-1"><?= $form->field($row, 'no_se_presento')->checkbox()->label(false) ?></td>
+            <td class="col-md-1"><?= $form->field($row, 'clasificacion_final')->textInput(['type' => 'number'])->label(false) ?></td>
+            <td class="col-md-2">
+                <?php
+                $buttonMsg = 'Guardar';
+                if (!empty($row->tiempo)) {
+                    $buttonMsg = 'Actualizar';
+                }
+                echo Html::submitButton($buttonMsg, ['class' => 'btn btn-success']);
+                echo $form->field($row, 'id_resultado_salto')->hiddenInput()->label(false);
+                ?>
+            </td>
+        </tr>
+        <tr>
+            <?php
+            foreach ($row->obstaculos as $obstaculo) {
+                ?>
+                <td>&nbsp;</td>
+                <?php
+            }
+            ?>
+        </tr>
+        </tbody>
+    </table>
+
+    <!--
+    <div class="col-md-2"><?= $row->orden_participacion . '. ' . ucwords(strtolower($row->caballoHasJinete->jinete->nombre_completo)) ?></div>
+    <div class="col-md-1"><?= ucwords(strtolower($row->caballoHasJinete->caballo->nombre)) ?></div>
 <?= $form->field($row, 'falta_obst')->textInput(['type' => 'number'])->label(false) ?>
 <?= $form->field($row, 'tiempo')->textInput(['type' => 'number', 'step' => '0.01'])->label(false) ?>
 <?= $form->field($row, 'faltas_tiempo')->textInput(['type' => 'number'])->label(false) ?>
@@ -39,8 +80,8 @@ $form = ActiveForm::begin([
 <?= $form->field($row, 'no_se_presento')->checkbox()->label(false) ?>
 <?= $form->field($row, 'clasificacion_final')->textInput(['type' => 'number'])->label(false) ?>
 
-<div class="col-md-1">
-    <?php
+    <div class="col-md-1">
+        <?php
     $buttonMsg = 'Guardar';
     if (!empty($row->tiempo)) {
         $buttonMsg = 'Actualizar';
@@ -48,8 +89,8 @@ $form = ActiveForm::begin([
     echo Html::submitButton($buttonMsg, ['class' => 'btn btn-success']);
     echo $form->field($row, 'id_resultado_salto')->hiddenInput()->label(false);
     ?>
-</div>
-
+    </div>
+-->
 <?php
 ActiveForm::end();
 //Pjax::end();
--- app/modules/backoffice/views/prueba-salto/calificarPrueba.php
+++ app/modules/backoffice/views/prueba-salto/calificarPrueba.php
@@ -12,27 +12,45 @@ $this->title = 'Calificar: ' . $model->nombre;
 $porCalificar = $model->getResultadoSaltos()->orderBy("-`clasificacion_final` DESC, orden_participacion ASC")->all();
 $pjaxId = 'savePrueba_' . $model->id_prueba;
 
-Pjax::begin([
-    'id' => $pjaxId,
-    'enablePushState' => false,
-    'options' => ['class' => 'row']
-]);
-
 ?>
 <script src="/assets/8b34cb6a/jquery.js?v=<?= time() ?>"></script>
 <script src="/assets/db9e8dc/yii.js?v=<?= time() ?>"></script>
 <script src="/assets/db9e8dc/yii.validation.js?v=<?= time() ?>"></script>
 <script src="/assets/db9e8dc/yii.activeForm.js?v=<?= time() ?>"></script>
 
-<div class="container">
+<style type="text/css">
+    .table-num-participacion {
+        width: 3%;
+    }
 
+    .table-jinete {
+        width: 15%;
+    }
+
+    .table-calificacion {
+        min-width: 100%;
+    }
+
+    .col-md-1 {
+        width: 15%;
+    }
+</style>
+
+<div class="container">
     <div class="row">
         <h1><?= Html::encode($this->title) ?></h1>
     </div>
-
+    <?php
+    Pjax::begin([
+        'id' => $pjaxId,
+        'enablePushState' => false,
+        'options' => ['class' => 'row']
+    ]);
+    ?>
     <div class="whole-wrap">
         <div class="container" style="margin-left: 0px;padding-left: 0px;">
-            <div class="row">`
+            <!--
+            <div class="row">
                 <div class="col-md-2">Jinete</div>
                 <div class="col-md-1">Caballo</div>
                 <div class="col-md-1">Falta Obst.</div>
@@ -47,6 +65,29 @@ Pjax::begin([
                 <div class="col-md-1">Clasificación Final</div>
                 <div class="col-md-2">&nbsp;</div>
             </div>
+            -->
+            <div class="row" data-spy="affix" data-offset-top="90" style="background-color: #ecf0f5; z-index: 99">
+                <table class="table-responsive" border="1px solid black">
+                    <thead>
+                    <tr>
+                        <td class="col-md-1">#</td>
+                        <td class="col-md-2" style="width 300px">Jinete</td>
+                        <td class="col-md-1">Caballo</td>
+                        <td class="col-md-1">Falta Obst.</td>
+                        <td class="col-md-1">Tiempo</td>
+                        <td class="col-md-1">Falta Tiempo</td>
+                        <td class="col-md-1">Falta Totales</td>
+                        <td class="col-md-1">Clasificación</td>
+                        <td class="col-md-1">Observaciones</td>
+                        <td class="col-md-1">Cantidad Rehusos</td>
+                        <td class="col-md-1">Eliminado</td>
+                        <td class="col-md-1">No se presento</td>
+                        <td class="col-md-1">Clasificación Final</td>
+                        <td class="col-md-2">&nbsp;</td>
+                    </tr>
+                    </thead>
+                </table>
+            </div>
             <?php
             /* @var $row \app\models\ResultadoSalto */
             foreach ($porCalificar as $row) {
@@ -58,12 +99,12 @@ Pjax::begin([
             ?>
         </div>
     </div>
+    <?php
+    Pjax::end();
+    ?>
     <script type="application/javascript" language="JavaScript">
         $(document).on('<?= $pjaxId ?>:beforeSend', function () {
             //alert('Seguro?');
         });
     </script>
-    <?php
-Pjax::end();
-?>
 </div>
